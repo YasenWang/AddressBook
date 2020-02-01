e584f7c49e969d0de953f4b29d99e41ecbfd3c11
YasenWang <00000000@wangyusheng.cn>
2020-02-01 22:28:00 +0800

    web war struts initialize

--- src/cn/wangyusheng/addressbook/main/dao/MysqlDao.java
+++ src/cn/wangyusheng/addressbook/main/dao/MysqlDao.java
@@ -1,7 +1,54 @@
 package cn.wangyusheng.addressbook.main.dao;
 
-public class MysqlDao {
-    MysqlDao(){
+import cn.wangyusheng.addressbook.main.vo.Contact;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class MysqlDao {
+    private Connection connection;
+    public MysqlDao(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+              "jdbc:mysql://127.0.0.1:3306/address_book?characterEncoding=UTF-8&serverTimezone=GMT%2B8",
+                    "root","wys19980107"
+            );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+    //获取数据信息之后，调用close方法，解除端口占用
+    private void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Contact[] read(){
+        ArrayList<Contact> contacts = new ArrayList<>();
+
+        Contact[] contacts_list = new Contact[contacts.size()];
+        return contacts.toArray(contacts_list);
+    }
+
+    public Contact[] fuzzyRead(String argv[]){
+        ArrayList<Contact> contacts = new ArrayList<>();
+
+        Contact[] contacts_list = new Contact[contacts.size()];
+        return contacts.toArray(contacts_list);
+    }
+
+    public boolean add(Contact contact){
+
+        return false;
+    }
+
+
 }
--- src/cn/wangyusheng/addressbook/test/AddressBookDebug.java
+++ src/cn/wangyusheng/addressbook/test/AddressBookDebug.java
@@ -1,8 +1,10 @@
 package cn.wangyusheng.addressbook.test;
 
+import cn.wangyusheng.addressbook.main.dao.MysqlDao;
+
 public class AddressBookDebug {
 
     public static void main(String[] args) {
-
+        new MysqlDao();
     }
 }
